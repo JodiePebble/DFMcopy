fb410391a6066c1f0a1ddb01ddd78be9c2eac1c8
JodiePebble <maddox.jodie@gmail.com>
2017-03-28 16:36:35 +0100

    added base path

--- serviceworker.js
+++ serviceworker.js
@@ -1,51 +1,52 @@
 var CACHE_NAME = 'gih-cache-v5';
+var BASE_PATH = '/DFMcopy/';
 var CACHED_URLS = [
   // Our HTML
-  'first.html',
-  'offline.html',
-  'second.html',
+  BASE_PATH + 'first.html',
+  BASE_PATH + 'offline.html',
+  BASE_PATH + 'second.html',
   // Stylesheets and fonts
-  'min-style.css',
-  'mystyles.css',
-  'styles.css',
+  BASE_PATH + 'min-style.css',
+  BASE_PATH + 'mystyles.css',
+  BASE_PATH + 'styles.css',
   // JavaScript
-  'material.js',
+  BASE_PATH + 'material.js',
   // Images
-  'appimages/paddy.jpg',
-  'appimages/android-icon-36x36.png',
-  'appimages/android-icon-48x48.png',
-  'appimages/android-icon-72x72.png',
-  'appimages/android-icon-96x96.png',
-  'appimages/android-icon-144x144.png',
-  'appimages/android-icon-192x192.png',
-  'appimages/dino.png',
-  'appimages/jack.jpg',
-  'appimages/favicon-16x16.png',
-  'appimages/favicon-32x32.png',
-  'appimages/favicon-96x96.png',
-  'appimages/favicon.ico',
-  'appimages/favicon.ico',
-  'appimages/favicon.ico',
-  'appimages/ms-icon-70x70.png',
-  'appimages/ms-icon-144x144.png',
-  'appimages/ms-icon-150x150.png',
-  'appimages/ms-icon-310x310.png',
-  'eventsimages/example-blog01.jpg',
-  'eventsimages/example-blog02.jpg',
-  'eventsimages/example-blog03.jpg',
-  'eventsimages/example-blog04.jpg',
-  'eventsimages/example-blog05.jpg',
-  'eventsimages/example-blog06.jpg',
-  'eventsimages/example-blog07.jpg',
-  'eventsimages/example-work01.jpg',
-  'eventsimages/example-work02.jpg',
-  'eventsimages/example-work03.jpg',
-  'eventsimages/example-work04.jpg',
-  'eventsimages/example-work05.jpg',
-  'eventsimages/example-work06.jpg',
-  'eventsimages/example-work07.jpg',
-  'eventsimages/example-work08.jpg',
-  'eventsimages/example-work09.jpg',
+  BASE_PATH + 'appimages/paddy.jpg',
+  BASE_PATH + 'appimages/android-icon-36x36.png',
+  BASE_PATH + 'appimages/android-icon-48x48.png',
+  BASE_PATH + 'appimages/android-icon-72x72.png',
+  BASE_PATH + 'appimages/android-icon-96x96.png',
+  BASE_PATH + 'appimages/android-icon-144x144.png',
+  BASE_PATH + 'appimages/android-icon-192x192.png',
+  BASE_PATH + 'appimages/dino.png',
+  BASE_PATH + 'appimages/jack.jpg',
+  BASE_PATH + 'appimages/favicon-16x16.png',
+  BASE_PATH + 'appimages/favicon-32x32.png',
+  BASE_PATH + 'appimages/favicon-96x96.png',
+  BASE_PATH + 'appimages/favicon.ico',
+  BASE_PATH + 'appimages/favicon.ico',
+  BASE_PATH + 'appimages/favicon.ico',
+  BASE_PATH + 'appimages/ms-icon-70x70.png',
+  BASE_PATH + 'appimages/ms-icon-144x144.png',
+  BASE_PATH + 'appimages/ms-icon-150x150.png',
+  BASE_PATH + 'appimages/ms-icon-310x310.png',
+  BASE_PATH + 'eventsimages/example-blog01.jpg',
+  BASE_PATH + 'eventsimages/example-blog02.jpg',
+  BASE_PATH + 'eventsimages/example-blog03.jpg',
+  BASE_PATH + 'eventsimages/example-blog04.jpg',
+  BASE_PATH + 'eventsimages/example-blog05.jpg',
+  BASE_PATH + 'eventsimages/example-blog06.jpg',
+  BASE_PATH + 'eventsimages/example-blog07.jpg',
+  BASE_PATH + 'eventsimages/example-work01.jpg',
+  BASE_PATH + 'eventsimages/example-work02.jpg',
+  BASE_PATH + 'eventsimages/example-work03.jpg',
+  BASE_PATH + 'eventsimages/example-work04.jpg',
+  BASE_PATH + 'eventsimages/example-work05.jpg',
+  BASE_PATH + 'eventsimages/example-work06.jpg',
+  BASE_PATH + 'eventsimages/example-work07.jpg',
+  BASE_PATH + 'eventsimages/example-work08.jpg',
+  BASE_PATH + 'eventsimages/example-work09.jpg',
 ];
 
 self.addEventListener('install', function(event) {
@@ -59,7 +60,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   var requestURL = new URL(event.request.url);
-  if (requestURL.pathname === 'first.html') {
+  if (requestURL.pathname ===  BASE_PATH + 'first.html') {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match('first.html').then(function(cachedResponse) {
@@ -73,7 +74,7 @@ self.addEventListener('fetch', function(event) {
     );
   } else if (
     CACHED_URLS.includes(requestURL.href) ||
-    CACHED_URLS.includes(requestURL.pathname) ){
+    CACHED_URLS.includes(requestURL.pathname)) {
     event.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return cache.match(event.request).then(function(response) {
